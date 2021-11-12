dd54d71fb0d9adfbf123215008b29e7344727653
Kaique Lira <Kaiqueliira@gmail.com>
2021-11-12 00:59:05 -0300

    Feito estilização global da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyles } from './styles/global';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <GlobalStyles/>
+    <h1>Hello World</h1>
     </div>
   );
 }
