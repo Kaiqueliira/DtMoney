4ecc4e483ecb71a1dbe8835cdfb61cad90d87133
Kaique Lira <Kaiqueliira@gmail.com>
2021-11-12 01:19:25 -0300

    criação e estilização  de componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from './components/Header';
 import { GlobalStyles } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyles/>
-    <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
   );
 }
 
